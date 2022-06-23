89bc73780bc5a4c26f7d5c348294ba2dc480a8a7
xcore995 <arman.p95@gmail.com>
2022-06-23 16:37:42 +0200

    douglas: update product shipping API for Q

--- device.mk
+++ device.mk
@@ -15,7 +15,7 @@
 #
 
 # Device launched with LP
-PRODUCT_SHIPPING_API_LEVEL := 22
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_l_mr1.mk)
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
