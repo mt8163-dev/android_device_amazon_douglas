a23fb2d6bb4892eb270dc5543825bca79e02f7ff
R0rt1z2 <rogerortizleal@gmail.com>
2022-07-23 00:46:58 +0200

    douglas: inherit core_64_bit.mk before full_base.mk
    * Caused weird conflicts with some zygote properties.

--- lineage_douglas.mk
+++ lineage_douglas.mk
@@ -15,8 +15,8 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
